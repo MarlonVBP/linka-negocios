ff9f16f3b051a65976b2b993104fb30edecae0ee
MarlonVBP <marlonvicctor13@gmail.com>
2024-07-01 20:12:23 -0300

    #

--- linka_negocios/src/app/routes.ts
+++ linka_negocios/src/app/routes.ts
@@ -1,6 +1,5 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {DetailsComponent} from './details/details.component';
 
 const routeConfig: Routes = [
     {
@@ -8,10 +7,10 @@ const routeConfig: Routes = [
       component: HomeComponent,
       title: 'Home page',
     },
-    {
-      path: 'details/:id',
-      component: DetailsComponent,
-      title: 'Home details',
-    },
+    // {
+    //   path: 'details/:id',
+    //   component: DetailsComponent,
+    //   title: 'Home details',
+    // },
   ];
   export default routeConfig;
