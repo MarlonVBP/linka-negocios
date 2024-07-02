21e4f75ad8c7fc4aaabab109345de22646c28bff
MarlonVBP <marlonvicctor13@gmail.com>
2024-07-02 16:25:32 -0300

    [ refactor ] - alteração do arquivo de rotas

--- linka_negocios/src/main.ts
+++ linka_negocios/src/main.ts
@@ -3,7 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 import {provideRouter} from '@angular/router';
-import routeConfig from './app/routes';
+import routeConfig from './app/app.routes';
 
 
 bootstrapApplication(AppComponent, {
