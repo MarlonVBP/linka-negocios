a8b5f3274e92a48ffe71a62083d9ee99abc4eda0
MarlonVBP <marlonvicctor13@gmail.com>
2024-07-09 07:52:23 -0300

    [ feat ] - criação da página de serviços

--- linka_negocios/src/app/app.routes.ts
+++ linka_negocios/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { ServicosPageComponent } from './servicos-page/servicos-page.component';
 
 export const routeConfig: Routes = [
     {
@@ -12,6 +13,11 @@ export const routeConfig: Routes = [
       path: 'login',
       component: LoginComponent,
       title: 'Login page',
+    },
+    {
+      path: 'servicos',
+      component: ServicosPageComponent,
+      title: 'Servicos page',
     },
     // {
     //   path: 'details/:id',
