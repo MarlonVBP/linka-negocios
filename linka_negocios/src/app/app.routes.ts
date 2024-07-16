26e4c9c6f22f7c48b7707b21ef1ecc3de365abc6
MarlonVBP <marlonvicctor13@gmail.com>
2024-07-16 16:02:19 -0300

    [ feat ] - tela de download finalizada

--- linka_negocios/src/app/app.routes.ts
+++ linka_negocios/src/app/app.routes.ts
@@ -6,52 +6,58 @@ import { ServicosPageComponent } from './servicos-page/servicos-page.component';
 import { SlidesShowComponent } from './components/public/slides-show/slides-show.component';
 import { SidebarAdminComponent } from './components/public/sidebar-admin/sidebar-admin.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { DownloadsComponent } from './downloads/downloads.component';
 
 export const routeConfig: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      title: 'Home page',
-    },
-    {
-      path: 'login',
-      component: LoginComponent,
-      title: 'Login page',
-    },
-    {
-      path: 'sobre-nos',
-      component: SobreNosComponent,
-      title: 'Sobre nós',
-    },
-    // {
-    //   path: '**',
-    //   component: HomeComponent,
-    //   title: 'Home page',
-    // },
-    {
-      path: 'sidebar-admin',
-      component: SidebarAdminComponent,
-      title: 'Menu Admin',
-    },
-    // {
-    //   path: 'details/:id',
-    //   component: DetailsComponent,
-    //   title: 'Home details',
-    // },
-    {
-      path: 'slide',
-      component: SlidesShowComponent,
-      title: 'Slide Show',
-    },
-    {
-      path: 'creat-post',
-      component: CreatePostComponent,
-      title: 'Post',
-    },
-    {
-      path: 'servicos',
-      component: ServicosPageComponent,
-      title: 'Serviços',
-    },
-  ];
-  export default routeConfig;
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login page',
+  },
+  {
+    path: 'sobre-nos',
+    component: SobreNosComponent,
+    title: 'Sobre nós',
+  },
+  {
+    path: 'sidebar-admin',
+    component: SidebarAdminComponent,
+    title: 'Menu Admin',
+  },
+  {
+    path: 'slide',
+    component: SlidesShowComponent,
+    title: 'Slide Show',
+  },
+  {
+    path: 'creat-post',
+    component: CreatePostComponent,
+    title: 'Post',
+  },
+  {
+    path: 'servicos',
+    component: ServicosPageComponent,
+    title: 'Serviços',
+  },
+  {
+    path: 'downloads',
+    component: DownloadsComponent,
+    title: 'Downloads',
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+  // {
+  //   path: 'details/:id',
+  //   component: DetailsComponent,
+  //   title: 'Home details',
+  // },
+];
+export default routeConfig;
