650819c6620c61104890536ee36729b0e3ab75b7
MarlonVBP <marlonvicctor13@gmail.com>
2024-07-18 16:54:33 -0300

    [ feat ] - implementação do modal de avaliações

--- linka_negocios/src/app/downloads/downloads.component.ts
+++ linka_negocios/src/app/downloads/downloads.component.ts
@@ -2,20 +2,61 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../components/public/footer/footer.component';
 import { SidebarClienteComponent } from '../components/public/sidebar-cliente/sidebar-cliente.component';
+import { AvaliacoesComponent } from "../components/public/avaliacoes/avaliacoes.component";
+import { ModalAvaliacoesComponent } from '../components/public/modal-avaliacoes/modal-avaliacoes.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-downloads',
   standalone: true,
-  imports: [FooterComponent, SidebarClienteComponent, CommonModule],
+  imports: [FooterComponent, SidebarClienteComponent, CommonModule, AvaliacoesComponent],
   templateUrl: './downloads.component.html',
   styleUrl: './downloads.component.scss'
 })
 export class DownloadsComponent {
-  expanded: boolean = false;
 
-  toggleExpand() {
-    this.expanded = !this.expanded;
+  constructor(public dialog: MatDialog) {
+
   }
+
+
+  avaliacoes = [
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+  ];
 
   perguntas: any[] = [
     {
@@ -44,6 +85,16 @@ export class DownloadsComponent {
       active: false
     }
   ];
+
+  openModal(): void {
+    this.dialog.open(ModalAvaliacoesComponent, {
+      minWidth: '70vw',
+      height: '70vh',
+      panelClass: 'custom-dialog-container',
+      data: this.avaliacoes
+    });
+
+  }
 
   toggleResposta(pergunta: any, respostaElement: HTMLElement) {
     pergunta.active = !pergunta.active;
--- linka_negocios/src/app/servicos-page/servicos-page.component.ts
+++ linka_negocios/src/app/servicos-page/servicos-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { SidebarClienteComponent } from '../components/public/sidebar-cliente/sidebar-cliente.component';
 import { FooterComponent } from '../components/public/footer/footer.component';
 import { ServicosCarouselComponent } from "../components/public/servicos-carousel/servicos-carousel.component";
+import { ModalAvaliacoesComponent } from '../components/public/modal-avaliacoes/modal-avaliacoes.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-servicos-page',
@@ -43,4 +45,57 @@ export class ServicosPageComponent {
       ]
     },
   ];
+
+  avaliacoes = [
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+    {
+      conteudo: "Depoimento que alguém escreveu destacando os benefícios ou ganhos que teve ao usar o seu produto/serviço.",
+      avatarUrl: "https://a.imagem.app/3qQLht.png",
+      nome: "Nome Sobrenome",
+      profissao: "Profissão",
+      empresa: "Empresa"
+    },
+  ];
+
+  constructor(public dialog: MatDialog) {
+
+  }
+
+
+  openModal(): void {
+    this.dialog.open(ModalAvaliacoesComponent, {
+      minWidth: '70vw',
+      height: '70vh',
+      panelClass: 'custom-dialog-container',
+      data: this.avaliacoes
+    });
+
+  }
 }
