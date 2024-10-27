4f8e95d673e5f4a672b2a6534a1203c70447c4cf
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-26 22:05:11 -0300

    :rocket: feat: organizando o layout da página de serviços

--- src/app/components/public/servicos-carousel/servicos-carousel.component.ts
+++ src/app/components/public/servicos-carousel/servicos-carousel.component.ts
@@ -1,28 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import { Card } from '../../../models/card';
 import { ServicosService } from '../../../services/servicos.service';
-import { SpinnerComponent } from "../spinner/spinner.component";
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-servicos-carousel',
   standalone: true,
   imports: [CommonModule, SpinnerComponent],
   templateUrl: './servicos-carousel.component.html',
-  styleUrls: ['./servicos-carousel.component.scss']
+  styleUrls: ['./servicos-carousel.component.scss'],
 })
-
 export class ServicosCarouselComponent implements AfterViewInit, OnDestroy {
   @ViewChild('carouselContainer') carouselContainer!: ElementRef;
 
   items: any[] = [];
-
   currentIndex = 0;
   lastIndex = 0;
   containerWidth = 0;
   intervalId: any;
   cards: Card[] = [];
   spinner: boolean = false;
+
+  private isDragging = false;
+  private startX = 0;
+  private deltaX = 0;
 
   constructor(private servicosServices: ServicosService) {
     this.spinner = true;
@@ -80,16 +88,18 @@ export class ServicosCarouselComponent implements AfterViewInit, OnDestroy {
     this.resetAutoSlide();
   }
 
-  startAutoSlide() {
-    this.intervalId = setInterval(() => {
-      this.next();
-    }, 12000);
-  }
-
   stopAutoSlide() {
     if (this.intervalId) {
       clearInterval(this.intervalId);
       this.intervalId = null;
+    }
+  }
+
+  startAutoSlide() {
+    if (!this.intervalId) {
+      this.intervalId = setInterval(() => {
+        this.next();
+      }, 12000);
     }
   }
 
@@ -99,10 +109,44 @@ export class ServicosCarouselComponent implements AfterViewInit, OnDestroy {
   }
 
   updateCard() {
-    this.cards = [{
-      text1: this.items[this.currentIndex].conteudo1,
-      text2: this.items[this.currentIndex].conteudo2,
-      text3: this.items[this.currentIndex].conteudo3,
-    }];
+    this.cards = [
+      {
+        text1: this.items[this.currentIndex].conteudo1,
+        text2: this.items[this.currentIndex].conteudo2,
+        text3: this.items[this.currentIndex].conteudo3,
+      },
+    ];
+  }
+
+  goToSlide(index: number): void {
+    this.currentIndex = index;
+    this.updateCard();
+    this.resetAutoSlide();
+  }
+
+  onTouchStart(event: TouchEvent): void {
+    this.isDragging = true;
+    this.startX = event.touches[0].clientX;
+    this.stopAutoSlide();
+  }
+
+  onTouchMove(event: TouchEvent): void {
+    if (!this.isDragging) return;
+    this.deltaX = event.touches[0].clientX - this.startX;
+  }
+
+  onTouchEnd(): void {
+    if (!this.isDragging) return;
+    this.isDragging = false;
+
+    // Verificar a direção do arrasto para toque
+    if (this.deltaX > 50) {
+      this.prev(); // Arrasto para a direita
+    } else if (this.deltaX < -50) {
+      this.next(); // Arrasto para a esquerda
+    }
+
+    this.deltaX = 0;
+    this.startAutoSlide(); // Reinicia o auto-slide
   }
 }
